534fe70a0905eb6f67fb6cbc933adad55b20114c
Matthew Manley <fake@email.com>
2018-05-18 18:55:22 -0400

    removed unnecessary line in build

--- build.cmd
+++ build.cmd
@@ -9,7 +9,6 @@ for /f "tokens=1-4 delims=/ " %%i in ("%date%") do (
 set datestr=%year%-%month%-%day%
 del ClientApp\BuildDate.ts
 echo export const BuildDate = "%datestr%";>> ClientApp\BuildDate.ts
-#echo. >> ClientApp\BuildDate.ts
 echo on
 
 call npm i
