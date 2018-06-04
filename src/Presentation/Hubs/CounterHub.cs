102d0e1b017da121fa2ab4df3383ccf9ecf5397b
Matthew Manley <fake@email.com>
2018-06-04 10:47:44 -0400

    added max room length

--- src/Presentation/Hubs/CounterHub.cs
+++ src/Presentation/Hubs/CounterHub.cs
@@ -11,6 +11,7 @@ namespace Presentation.Hubs
     public class CounterHub : Hub
     {
         private readonly ICounterRepository _counterRepository;
+        private const int MaxRoomLength = 16;
 
         public CounterHub(ICounterRepository counterRepository)
         {
@@ -19,6 +20,9 @@ namespace Presentation.Hubs
 
         public async Task Join(string room = "")
         {
+            if (room.Length > MaxRoomLength)
+                room = room.Substring(0, MaxRoomLength);
+
             var addToGroupTask = Groups.AddToGroupAsync(Context.ConnectionId, room);
             var count = await _counterRepository.GetValueAsync(room);
             await Task.WhenAll(
@@ -28,6 +32,9 @@ namespace Presentation.Hubs
 
         public async Task Increment(string room = "")
         {
+            if (room.Length > MaxRoomLength)
+                room = room.Substring(0, MaxRoomLength);
+
             await Task.WhenAll(
                 _counterRepository.IncrementValueAsync(room),
                 Clients.OthersInGroup(room).SendAsync("Increment")
@@ -36,6 +43,9 @@ namespace Presentation.Hubs
 
         public async Task Decrement(string room = "")
         {
+            if (room.Length > MaxRoomLength)
+                room = room.Substring(0, MaxRoomLength);
+
             await Task.WhenAll(
                 _counterRepository.DecrementValueAsync(room),
                 Clients.OthersInGroup(room).SendAsync("Decrement")
